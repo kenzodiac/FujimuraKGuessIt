d0935d14efcc36c1d13f52e41bea6032a6c74705
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 16:02:10 -0700

    final push

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 //Last accessed: 10-19-2022
 //GitHub Challenge # 8 - Guess It
 //This program has the user input numbers to guess the right answer. The numbers are randomly generated. There are multiple difficulty levels that expand the number range to guess from.
-//Peer Reviewed by:
+//Peer Reviewed by: Chris Martinez
+//This is my take on the game. the game itself meets all of the criteria requested, plus the xtra credit that includes the Custom Guess option. 
+//I enjoyed playing each level, however I was not fond of the word selection by the Programmer "Your guess is less than my number" its more a personal dislike than the actual meaning of what the sentence is trying to convey. 
+//Adding a fun twist with the wording would be nice. Besides that' I think the program exceeds the expectations of the requirements. 
 
 Console.Clear();
 //Global variables
